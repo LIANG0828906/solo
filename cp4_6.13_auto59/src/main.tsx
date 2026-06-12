2ab640ff3b74357dac392152e64124b6e5cab5c3
LIANG <3047387440@qq.com>
2026-06-13 02:27:21 +0800

    Round 1: Upload auto59 as cp4_6.13_auto59

---
+++ cp4_6.13_auto59/src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import VisualizerApp from './visualizerApp';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <VisualizerApp />
+  </React.StrictMode>,
+);
