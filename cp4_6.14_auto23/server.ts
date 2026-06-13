d9d67181a7c828a26442563690456789d4f5caff
LIANG <3047387440@qq.com>
2026-06-14 02:37:49 +0800

    Round 3: Upload auto23 as cp4_6.14_auto23

--- cp4_6.14_auto23/server.ts
+++ cp4_6.14_auto23/server.ts
@@ -1,4 +1,5 @@
-import express, { Request, Response, NextFunction } from 'express';
+import express from 'express';
+import type { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import { v4 as uuidv4 } from 'uuid';
 
