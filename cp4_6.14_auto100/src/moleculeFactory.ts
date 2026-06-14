c9f49a7881bc80e0412bb6ed391b5250262a9b47
LIANG <3047387440@qq.com>
2026-06-14 16:30:53 +0800

    Round 3: Upload auto100 as cp4_6.14_auto100

--- cp4_6.14_auto100/src/moleculeFactory.ts
+++ cp4_6.14_auto100/src/moleculeFactory.ts
@@ -19,105 +19,164 @@ export interface MoleculeData {
   bonds: Bond[];
 }
 
-export const atomTypes: Record<string, AtomType> = {
+export const ATOM_TYPES: Record<string, AtomType> = {
   H: { name: 'hydrogen', color: 0xffffff, radius: 0.3 },
   C: { name: 'carbon', color: 0x808080, radius: 0.5 },
   O: { name: 'oxygen', color: 0xff0000, radius: 0.6 }
 };
 
-const water: MoleculeData = {
-  atoms: [
-    { type: 'O', position: [0, 0, 0] },
-    { type: 'H', position: [0.96, 0, 0] },
-    { type: 'H', position: [-0.24, 0.93, 0] }
-  ],
-  bonds: [
-    { from: 0, to: 1 },
-    { from: 0, to: 2 }
-  ]
-};
+export type MoleculeName = 'water' | 'methane' | 'glucose';
 
-const methane: MoleculeData = {
-  atoms: [
-    { type: 'C', position: [0, 0, 0] },
-    { type: 'H', position: [1.09, 0, 0] },
-    { type: 'H', position: [-0.36, 0.72, 0.72] },
-    { type: 'H', position: [-0.36, -0.72, 0.72] },
-    { type: 'H', position: [-0.36, 0, -1.02] }
-  ],
-  bonds: [
-    { from: 0, to: 1 },
-    { from: 0, to: 2 },
-    { from: 0, to: 3 },
-    { from: 0, to: 4 }
-  ]
-};
-
-const glucose: MoleculeData = {
-  atoms: [
-    { type: 'O', position: [0, 1.4, 0] },
-    { type: 'C', position: [0, 0, 0] },
-    { type: 'C', position: [1.3, -0.4, 0.3] },
-    { type: 'C', position: [1.6, -1.4, 1.4] },
-    { type: 'C', position: [0.5, -2.2, 1.9] },
-    { type: 'C', position: [-0.8, -1.8, 1.6] },
-    { type: 'C', position: [-1.2, -0.8, 0.5] },
-    { type: 'O', position: [-1.5, 0.2, 0.8] },
-    { type: 'O', position: [2.3, 0.3, 0.6] },
-    { type: 'O', position: [2.8, -1.8, 1.6] },
-    { type: 'O', position: [0.8, -3.2, 2.7] },
-    { type: 'O', position: [-1.8, -2.5, 2.0] },
-    { type: 'H', position: [0.5, 1.8, 0.3] },
-    { type: 'H', position: [-0.5, 0.3, -0.9] },
-    { type: 'H', position: [1.3, 0.2, -0.7] },
-    { type: 'H', position: [2.7, 0.7, 0.2] },
-    { type: 'H', position: [1.7, -0.9, 2.4] },
-    { type: 'H', position: [3.2, -2.2, 1.2] },
-    { type: 'H', position: [0.4, -1.7, 2.9] },
-    { type: 'H', position: [1.2, -3.8, 3.2] },
-    { type: 'H', position: [-1.5, -1.3, 2.5] },
-    { type: 'H', position: [-2.2, -2.9, 2.3] },
-    { type: 'H', position: [-2.0, 0.5, 0.3] },
-    { type: 'H', position: [-1.0, -1.3, -0.5] }
-  ],
-  bonds: [
-    { from: 0, to: 1 },
-    { from: 1, to: 2 },
-    { from: 2, to: 3 },
-    { from: 3, to: 4 },
-    { from: 4, to: 5 },
-    { from: 5, to: 6 },
-    { from: 6, to: 1 },
-    { from: 6, to: 7 },
-    { from: 2, to: 8 },
-    { from: 3, to: 9 },
-    { from: 4, to: 10 },
-    { from: 5, to: 11 },
-    { from: 0, to: 12 },
-    { from: 1, to: 13 },
-    { from: 2, to: 14 },
-    { from: 8, to: 15 },
-    { from: 3, to: 16 },
-    { from: 9, to: 17 },
-    { from: 4, to: 18 },
-    { from: 10, to: 19 },
-    { from: 5, to: 20 },
-    { from: 11, to: 21 },
-    { from: 7, to: 22 },
-    { from: 6, to: 23 }
-  ]
-};
-
-const molecules: Record<string, MoleculeData> = {
-  water,
-  methane,
-  glucose
-};
-
-export function getMoleculeData(name: string): MoleculeData {
-  return molecules[name] || water;
+function createWater(): MoleculeData {
+  return {
+    atoms: [
+      { type: 'O', position: [0, 0, 0] },
+      { type: 'H', position: [0.96, 0, 0] },
+      { type: 'H', position: [-0.24, 0.93, 0] }
+    ],
+    bonds: [
+      { from: 0, to: 1 },
+      { from: 0, to: 2 }
+    ]
+  };
 }
 
-export function getMoleculeNames(): string[] {
-  return Object.keys(molecules);
+function createMethane(): MoleculeData {
+  const d = 1.09;
+  const sqrt2over3 = Math.sqrt(2) / 3;
+  const sqrt8over9 = Math.sqrt(8 / 9);
+  const onethird = 1 / 3;
+  return {
+    atoms: [
+      { type: 'C', position: [0, 0, 0] },
+      { type: 'H', position: [d, 0, 0] },
+      { type: 'H', position: [-d / 3, d * sqrt8over9, 0] },
+      { type: 'H', position: [-d / 3, -d * sqrt2over3, d * sqrt2over3] },
+      { type: 'H', position: [-d / 3, -d * sqrt2over3, -d * sqrt2over3] }
+    ],
+    bonds: [
+      { from: 0, to: 1 },
+      { from: 0, to: 2 },
+      { from: 0, to: 3 },
+      { from: 0, to: 4 }
+    ]
+  };
+}
+
+function createGlucose(): MoleculeData {
+  const atoms: Atom[] = [];
+  const bonds: Bond[] = [];
+
+  const rCC = 1.54;
+  const rCO = 1.43;
+  const rCH = 1.09;
+  const rOH = 0.96;
+  const sin60 = Math.sin(Math.PI / 3);
+  const cos60 = Math.cos(Math.PI / 3);
+  const sin120 = Math.sin(2 * Math.PI / 3);
+  const cos120 = Math.cos(2 * Math.PI / 3);
+
+  const ringZ = 0.3;
+
+  atoms.push({ type: 'C', position: [0, 0, 0] });
+  atoms.push({ type: 'C', position: [rCC, 0, 0] });
+  atoms.push({ type: 'C', position: [rCC + rCC * cos60, rCC * sin60, 0] });
+  atoms.push({ type: 'C', position: [rCC, 2 * rCC * sin60, 0] });
+  atoms.push({ type: 'C', position: [0, 2 * rCC * sin60, 0] });
+  atoms.push({ type: 'O', position: [-rCO * cos60, rCC * sin60, 0] });
+  atoms.push({ type: 'C', position: [-rCO * cos60 - rCC * cos60, rCC * sin60 + rCC * sin60, 0] });
+
+  const idx = {
+    C1: 0, C2: 1, C3: 2, C4: 3, C5: 4, O1: 5, C6: 6
+  };
+
+  bonds.push(
+    { from: idx.C1, to: idx.C2 },
+    { from: idx.C2, to: idx.C3 },
+    { from: idx.C3, to: idx.C4 },
+    { from: idx.C4, to: idx.C5 },
+    { from: idx.C5, to: idx.O1 },
+    { from: idx.O1, to: idx.C1 },
+    { from: idx.C5, to: idx.C6 }
+  );
+
+  function addH(x: number, y: number, z: number) {
+    atoms.push({ type: 'H', position: [x, y, z] });
+    return atoms.length - 1;
+  }
+
+  function addO(x: number, y: number, z: number) {
+    atoms.push({ type: 'O', position: [x, y, z] });
+    return atoms.length - 1;
+  }
+
+  const hC1 = addH(-sin60 * rCH, -cos60 * rCH, ringZ);
+  bonds.push({ from: idx.C1, to: hC1 });
+  const oC1 = addO(-sin60 * rCO, -cos60 * rCO, -ringZ);
+  bonds.push({ from: idx.C1, to: oC1 });
+  const hoC1 = addH(oC1.position[0] - 1, oC1.position[1], -ringZ);
+  bonds.push({ from: oC1, to: hoC1 });
+
+  const hC2 = addH(rCC + sin60 * rCH, -cos60 * rCH, -ringZ);
+  bonds.push({ from: idx.C2, to: hC2 });
+  const oC2 = addO(rCC + sin60 * rCO, -cos60 * rCO, ringZ);
+  bonds.push({ from: idx.C2, to: oC2 });
+  const hoC2 = addH(oC2.position[0] + 0.5, oC2.position[1] - 0.7, ringZ);
+  bonds.push({ from: oC2, to: hoC2 });
+
+  const hC3 = addH(rCC + rCC * cos60 + cos60 * rCH, rCC * sin60 + sin60 * rCH, ringZ);
+  bonds.push({ from: idx.C3, to: hC3 });
+  const oC3 = addO(rCC + rCC * cos60 + cos60 * rCO, rCC * sin60 + sin60 * rCO, -ringZ);
+  bonds.push({ from: idx.C3, to: oC3 });
+  const hoC3 = addH(oC3.position[0] + 0.7, oC3.position[1] + 0.5, -ringZ);
+  bonds.push({ from: oC3, to: hoC3 });
+
+  const hC4 = addH(rCC + sin60 * rCH, 2 * rCC * sin60 + cos60 * rCH, -ringZ);
+  bonds.push({ from: idx.C4, to: hC4 });
+  const oC4 = addO(rCC + sin60 * rCO, 2 * rCC * sin60 + cos60 * rCO, ringZ);
+  bonds.push({ from: idx.C4, to: oC4 });
+  const hoC4 = addH(oC4.position[0] + 0.5, oC4.position[1] + 0.7, ringZ);
+  bonds.push({ from: oC4, to: hoC4 });
+
+  const hC5 = addH(-sin60 * rCH, 2 * rCC * sin60 + cos60 * rCH, ringZ);
+  bonds.push({ from: idx.C5, to: hC5 });
+
+  const hC6a = addH(idx.C6 ? atoms[idx.C6].position[0] - rCH : 0,
+                     (atoms[idx.C6]?.position[1] ?? 0) + rCH * 0.5,
+                     -ringZ);
+  bonds.push({ from: idx.C6, to: hC6a });
+  const hC6b = addH((atoms[idx.C6]?.position[0] ?? 0) - rCH * 0.5,
+                     (atoms[idx.C6]?.position[1] ?? 0) + rCH,
+                     ringZ);
+  bonds.push({ from: idx.C6, to: hC6b });
+  const oC6 = addO((atoms[idx.C6]?.position[0] ?? 0) + rCO * 0.5,
+                    (atoms[idx.C6]?.position[1] ?? 0) + rCO * 0.8,
+                    -ringZ * 0.5);
+  bonds.push({ from: idx.C6, to: oC6 });
+  const hoC6 = addH(oC6.position[0] + 0.7, oC6.position[1] + 0.5, -ringZ * 0.5);
+  bonds.push({ from: oC6, to: hoC6 });
+
+  return { atoms, bonds };
+}
+
+const factories: Record<MoleculeName, () => MoleculeData> = {
+  water: createWater,
+  methane: createMethane,
+  glucose: createGlucose
+};
+
+export function createMolecule(name: MoleculeName): MoleculeData {
+  const factory = factories[name];
+  if (!factory) {
+    throw new Error(`Unknown molecule: ${name}`);
+  }
+  return factory();
+}
+
+export function getAtomType(type: string): AtomType {
+  return ATOM_TYPES[type] ?? ATOM_TYPES.C;
+}
+
+export function colorToHex(color: number): string {
+  return '#' + color.toString(16).padStart(6, '0').toUpperCase();
 }
