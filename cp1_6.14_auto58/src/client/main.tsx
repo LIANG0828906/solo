0c7bbc941f3c715c2047069b24c3518897c6ee36
LIANG0828906 <3047387440@qq.com>
2026-06-14 12:40:00 +0800

    Round 2: Upload auto58 as cp1_6.14_auto58

--- cp1_6.14_auto58/src/client/main.tsx
+++ cp1_6.14_auto58/src/client/main.tsx
@@ -1,16 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { AppProvider } from './context/AppContext';
+import App from './App';
 import './styles/global.css';
-
-const App: React.FC = () => {
-  return (
-    <div className="app">
-      <h1>团队OKR管理系统</h1>
-    </div>
-  );
-};
 
 const container = document.getElementById('root');
 
@@ -22,10 +13,6 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AppProvider>
-        <App />
-      </AppProvider>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
