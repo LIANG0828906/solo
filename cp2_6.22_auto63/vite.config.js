dd186510b39144e6f27bcb2133495de5b8685de5
LIANG <3047387440@qq.com>
2026-06-22 13:57:21 +0800

    Round 1: Upload auto63 as cp2_6.22_auto63

---
+++ cp2_6.22_auto63/vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  plugins: [],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+  build: {
+    target: 'esnext',
+  },
+});
